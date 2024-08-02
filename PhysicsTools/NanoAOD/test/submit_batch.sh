ed53f3ade426d8649651b98ae8393e335affe496
emily-tsai11 <emily.tsai11@gmail.com>
2024-08-03 00:43:07 +0200

    add batch submission script

---
+++ PhysicsTools/NanoAOD/test/submit_batch.sh
@@ -0,0 +1,22 @@
+source submit_function.sh
+
+submitBatch() {
+  cat $YEAR\_$TYPE.list | while read LINE 
+  do
+    OPTS=($LINE)
+    DATASET=${OPTS[0]}
+    SAMPLE=${OPTS[1]}
+    submitDataset
+  done
+}
+
+for year in 2016postVFP 2016preVFP 2017 2018
+do
+  for type in data MC
+  do
+    YEAR=$year
+    TYPE=$type
+    CFG=/eos/user/e/etsai/workspace/TopLFV_ReNano_CMSSW_13_2_2/src/PhysicsTools/NanoAOD/test/topNano_v9_1_1_${YEAR}UL_${TYPE}_cfg.py
+    submitBatch
+  done
+done
---
+++ PhysicsTools/NanoAOD/test/topNano_v9_1_1_2016preVFPUL_data_cfg.py
@@ -0,0 +1,88 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: NANO --python_filename topNano_v9-1-1_2016UL_data_cfg.py --fileout file:tree.root -s NANO --data --conditions 106X_dataRun2_v35 --era Run2_2016,run2_nanoAOD_106Xv2 --eventcontent NANOAOD --datatier NANOAOD --customise_commands=process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)));process.MessageLogger.cerr.FwkReport.reportEvery=1000;process.NANOAODoutput.fakeNameForCrab=cms.untracked.bool(True) --nThreads 2 -n -1 --no_exec
+
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
+from Configuration.Eras.Modifier_run2_nanoAOD_106Xv2_cff import run2_nanoAOD_106Xv2
+
+process = cms.Process('NANO',Run2_2016,run2_nanoAOD_106Xv2)
+
+# Import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch///store/data/Run2016F/MuonEG/MINIAOD/UL2016_MiniAODv2-v2/140000/E7392AB4-1387-3946-B3DD-8C446BF58B1A.root'), # postVFP
+    # fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch///store/data/Run2016B/MuonEG/MINIAOD/ver1_HIPM_UL2016_MiniAODv2-v2/120000/229FBC5A-7299-8642-881A-D7DE49FE5AF3.root'), # preVFP
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet()
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('NANO nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAOD'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:2016_data.root'),
+    outputCommands = process.NANOAODEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_dataRun2_v35', '')
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.nanoSequence)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step,process.NANOAODoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(2)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+
+# Customisation of the process
+from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeCommon
+process = nanoAOD_customizeCommon(process)
+
+# Customisation from command line
+
+process.add_(cms.Service('InitRootHandlers', EnableIMT = cms.untracked.bool(False)));process.MessageLogger.cerr.FwkReport.reportEvery=1000;process.NANOAODoutput.fakeNameForCrab=cms.untracked.bool(True)
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
